37c6b39f61a049e2f1be2177a60e4484bb39e467
Edward7676 <46484847+Edwad7676@users.noreply.github.com>
2020-03-16 09:50:07 +0700

    updated html 16/03/2020

--- server.py
+++ server.py
@@ -5,6 +5,14 @@ app = Flask(__name__)
 def home():
 
     return render_template('index.html')
-
+@app.route('/blog')
+def blog():
+    return render_template('blog.html')
+@app.route('/single')
+def single():
+    return render_template('single.html')
+@app.route('/styles')
+def style():
+    return render_template('styles.html')
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5005, debug=True)
